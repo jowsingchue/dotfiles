d406514060f1e3c7f25f960a773acafeec17ab56
wootsaejao <wootsaejao@gmail.com>
2016-03-01 18:37:16 +0700

    aliases for virtualenv

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -29,3 +29,11 @@ gitloop() {
     cd ..;
   done;
 }
+
+# raspi
+alias connect_raspi_ttl="sudo screen /dev/ttyUSB0 115200"
+alias connect_raspi_ssh="ssh pi@192.168.0.80"
+
+# virtualenv
+# alias sv="source .venv/bin/activate"
+alias sv='source .venv/bin/activate; export PS1="(${PWD##*/}-venv)$_OLD_VIRTUAL_PS1"'
