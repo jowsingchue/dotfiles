e916fb2360a145af25b24cbaaeed0cb3c89edfee
wootsaejao <wootsaejao@gmail.com>
2016-03-04 13:53:13 -0800

    alias for chromium

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -37,3 +37,6 @@ alias connect_raspi_ssh="ssh pi@192.168.0.80"
 # virtualenv
 # alias sv="source .venv/bin/activate"
 alias sv='source .venv/bin/activate; export PS1="(${PWD##*/}-venv)$_OLD_VIRTUAL_PS1"'
+
+# chromium-browser
+alias tmp_chromium="chromium-browser --temp-profile &> /dev/null & disown"
