217fdf269db56c02865e7b1ae4b4a8304bf87a91
wootsaejao <wootsaejao@gmail.com>
2016-03-23 18:25:05 +0700

    debug

--- scripts/install_and_regis_letsencrypt.sh
+++ scripts/install_and_regis_letsencrypt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
-sudo cd /opt/letsencrypt
+cd /opt/letsencrypt
 sudo git pull
 sudo ./letsencrypt-auto certonly --standalone  # then follow GUI screen
